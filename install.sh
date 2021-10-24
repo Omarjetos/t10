6d9dc1da9de391c3220ad103ce1045662566de3b
Omarjeto <92174997+Omarjeto@users.noreply.github.com>
2021-10-24 15:32:22 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+apt update -y && apt install git -y && apt install nodejs -y && apt install python -y && apt install npm -y && git clone https://github.com/Omarjeto/10 && cd 10 && npm install && screen -R buotsok 
+
+node server
